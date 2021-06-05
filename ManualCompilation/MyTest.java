e6808523351fda04450b891e637ae91731b84be5
narendranmuraliFS <narendranmurali1030learning@gmail.com>
2021-06-05 12:30:47 +0530

    first commit

---
+++ ManualCompilation/MyTest.java
@@ -0,0 +1,8 @@
+public class MyTest{
+public static void main(String[] args){
+System.out.println("Hello World !!!");
+System.out.println(args.length);
+System.out.println(args[0] + " " + args[1]);
+
+}
+}
